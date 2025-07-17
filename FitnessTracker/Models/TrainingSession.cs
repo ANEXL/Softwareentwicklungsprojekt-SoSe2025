f4dfc868491e70dd47bf399b2738d37c2e38043d
antonklh <anton.kalch@gmail.com>
2025-07-17 14:34:46 +0200

    Grundlegende Klassen

---
+++ FitnessTracker/Models/TrainingSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+
+namespace Ableger.Models
+{
+    public class TrainingSession
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now; // Datum und Uhrzeit des Trainings
+        public string Notes { get; set; } = string.Empty; // Optional: Notizen zum Training
+    }
+}
