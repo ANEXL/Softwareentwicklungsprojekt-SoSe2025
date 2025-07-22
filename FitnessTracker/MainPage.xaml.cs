801bcd3ab15edaab550d1585f52fdbe582bb57d4
AM-67 <mobil.felber.alex@gmail.com>
2025-07-22 15:32:44 +0200

    added Workout Page with parameter

--- FitnessTracker/MainPage.xaml.cs
+++ FitnessTracker/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿//using static Android.Print.PrintAttributes;
-//using static Java.Util.Jar.Attributes;
+﻿
 namespace FitnessTracker
 {
     public partial class MainPage : ContentPage
@@ -10,29 +9,34 @@ namespace FitnessTracker
         {
             InitializeComponent();
         }
+        // This variable keeps track of the number of workouts added
         int workoutCount = 0;
 
         private void AddWorkoutBtn_Clicked(object sender, EventArgs e)
         {
             workoutCount++;
+
+            // Generate a unique name for the new workout button
             string btnName = $"Workout {workoutCount}";
 
+            // Create a new button dynamically
             Button newworkout = new Button();
 
 
-
+            // Set properties for the new button
             MainStack.Children.Add(newworkout);
-
             newworkout.Text = btnName;
             newworkout.BackgroundColor = Color.FromArgb("#4CAF50");
             newworkout.TextColor = Color.Parse("red");
             newworkout.FontSize = 20;
             newworkout.Padding = 10;
             newworkout.Margin = 20;
+
+            // Add an event handler for the button click
             newworkout.Clicked += (s, args) =>
             {
-                //Navigation.PushAsync(new AddWorkoutPage());
-                newworkout.Text = "Clicked!";
+                // Navigate to the WorkoutPage with the button name as a parameter
+                Navigation.PushAsync(new WorkoutPage(btnName));
             };
 
 
