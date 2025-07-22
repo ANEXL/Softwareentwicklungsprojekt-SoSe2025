71aef2d410f719c60936197e04eb3f4eab71b8e2
antonklh <anton.kalch@gmail.com>
2025-07-22 14:51:45 +0200

    Klassen an Relationenmodell fuer DB angepasst

---
+++ FitnessTracker/Models/Uebung.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace FitnessTracker.Models
+{
+    [Table("UebungsTbl")] // Mappt die Klasse auf den Tabellennamen
+    public class Uebung
+    {
+        [PrimaryKey, AutoIncrement]
+        [Column("UebungsID")] // Mappt die Property auf den Primärschlüssel-Spaltennamen
+        public int UebungID { get; set; }
+
+        [Column("Uebung")] // Mappt die Property auf den Spaltennamen im Diagramm
+        public string Bezeichnung { get; set; } = string.Empty;
+    }
+}
