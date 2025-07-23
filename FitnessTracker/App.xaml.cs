b1bf10ce7b1de0baaa2002fca89cd89eff2cc945
antonklh <anton.kalch@gmail.com>
2025-07-23 16:54:46 +0200

    WorkoutPage-Funktionalitaet

--- FitnessTracker/App.xaml.cs
+++ FitnessTracker/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace FitnessTracker
+﻿using System.Threading.Tasks;
+using FitnessTracker.Services;
+
+namespace FitnessTracker
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            var dbService = MauiProgram.Current.Services.GetService<DatabaseService>();
+            if (dbService != null)
+                Task.Run(async () => await dbService.InitialisiereDatenbankAsync());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- FitnessTracker/MauiProgram.cs
+++ FitnessTracker/MauiProgram.cs
@@ -1,25 +1,35 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using FitnessTracker.Services;
+using FitnessTracker;
+using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
-namespace FitnessTracker
+namespace FitnessTracker;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp Current { get; private set; }
+
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .UseMauiCommunityToolkit();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
+        builder.Services.AddSingleton<DatabaseService>();
 
-            return builder.Build();
-        }
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<WorkoutPage>();
+
+        Current = builder.Build();
+        return Current;
     }
 }
